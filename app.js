0fa7fe79d40bb314ec08e5523b71d52a7296faf6
Arnau <arnauma16@gmail.com>
2019-10-13 12:16:16 +0200

    decimal minor bugs fixed

--- app.js
+++ app.js
@@ -1,159 +1,121 @@
-const scOutput = document.querySelector("#screenOutput");
-const btnList = document.querySelectorAll(".allBtns");
-const clearBtn = document.querySelector(".clear");
-const deleteBtn = document.querySelector(".delete");
-const operations = document.querySelectorAll(".operators");
-const equal = document.querySelector("#equal");
-
+// INIT VARIABLES
+const scOutput = document.querySelector('#screenOutput');
+const btnList = document.querySelectorAll('.allBtns');
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+const operations = document.querySelectorAll('.operators');
+const equal = document.querySelector('#equal');
 
 let firstValue;
 let secondValue;
 let symbol;
-let result;
 
+/* ---------------------------------------------- ADD EVENT LISTENERS ------------------------------------------ */
 
-// btnList.forEach(btn => {
-//     btn.addEventListener("click", () => {
-//         const characters = Array.from(scOutput.textContent);
-
-//         console.log(!(characters.includes(".")));
-//         console.log(btn.textContent);
-//         if (characters[0] === "0") {
-//             scOutput.textContent = "";
-//         }
-//         scOutput.textContent += btn.textContent;
-
-//     })
-// });
-
-// NEED TO WORK ON .NUM SCENARIO NO MORE, NOW TOFIXED
-
-btnList.forEach(btn => {
-    btn.addEventListener("click", () => {
+// ADD FUNCTIONALITY TO NUMBER BUTTONS
+btnList.forEach((btn) => {
+    btn.addEventListener('click', () => {
         const characters = Array.from(scOutput.textContent);
 
-        console.log(!(characters.includes(".")));
+        console.log(!characters.includes('.'));
         console.log(btn.textContent);
-        if (characters[0] === "0" && characters.length === 1) {
-            scOutput.textContent = "";
+        if (characters[0] === '0' && characters.length === 1) {
+            scOutput.textContent = '';
         }
 
-        if (scOutput.textContent === "" && btn.textContent == "." && !(characters.includes("."))) {
-            scOutput.textContent = "0";
+        if (scOutput.textContent === '' && btn.textContent === '.' && !characters.includes('.')) {
+            scOutput.textContent = '0';
         }
-        scOutput.textContent += btn.textContent;
+
+        if (characters.includes('.') && btn.textContent === '.') {
+            return;
+        } else {
+            scOutput.textContent += btn.textContent;
+        }
 
     });
 });
 
-
-
-
-operations.forEach(item => {
-    item.addEventListener("click", () => {
+// CATCH OPERATION
+operations.forEach((item) => {
+    item.addEventListener('click', () => {
         firstValue = getValue();
         symbol = item.textContent;
         clearOutput();
-        console.log(firstValue);
-        console.log(symbol);
     });
 });
 
-clearBtn.addEventListener("click", clearOutput);
+// CLEAR OUTPUT
+clearBtn.addEventListener('click', clearOutput);
 
-deleteBtn.addEventListener("click", deleteNumberFromOutput);
+// DELETE NUMBER / CHARACTER FORM OUTPUT
+deleteBtn.addEventListener('click', deleteNumberFromOutput);
 
-equal.addEventListener("click", () => {
+// PERFORM OPERATION
+equal.addEventListener('click', () => {
     secondValue = getValue();
     console.log(secondValue);
 
     doCalculations(firstValue, secondValue, symbol);
-    console.log(result);
+    strResult = result.toString();
     scOutput.textContent = result;
 });
 
-function clearOutput() {
-    scOutput.textContent = "0";
-};
+/* ---------------------------------------------- ADD EVENT LISTENERS END --------------------------------------- */
 
+/* ------------------------------------------------- DECLARE FUNCTIONS ------------------------------------------ */
+
+function clearOutput() {
+    scOutput.textContent = '0';
+}
 
 function deleteNumberFromOutput() {
     const characters = Array.from(scOutput.textContent);
     characters.pop();
 
-    if (characters[characters.length - 1] === ".") {
+    if (characters[characters.length - 1] === '.') {
         characters.pop();
-    };
-    const joinedString = characters.join("");
+    }
+    const joinedString = characters.join('');
 
     scOutput.textContent = joinedString;
-};
+}
 
 function getValue() {
     const value = scOutput.textContent;
-    console.log("Value " + value);
-
     return value;
-};
+}
 
 function doCalculations(value1, value2, operation) {
-    const characters1 = Array.from(value1);
-    const characters2 = Array.from(value2);
-    let parsed1;
-    let parsed2;
+    let resultStr;
 
-    console.log(characters1);
-
-    if (characters1.includes(".") || characters2.includes(".")) {
-        parsed1 = parseFloat(value1);
-        parsed2 = parseFloat(value2);
-        parsed1.toFixed(2);
-        parsed2.toFixed(2);
+    if (value1.includes('.') || value2.includes('.')) {
+        value1 = parseFloat(value1);
+        value2 = parseFloat(value2);
     } else {
-        parsed1 = parseInt(value1);
-        parsed2 = parseInt(value2);
+        value1 = parseInt(value1);
+        value2 = parseInt(value2);
     }
 
-    console.log(parsed1.toFixed(2));
-
-
-    if (operation === "+") {
-        result = parsed1 + parsed2;
+    if (operation === '+') {
+        result = value1 + value2;
     }
 
-    if (operation === "-") {
-        result = parsed1 - parsed2;
+    if (operation === '-') {
+        result = value1 - value2;
     }
-    if (operation === "*") {
-        result = parsed1 * parsed2;
+    if (operation === '*') {
+        result = value1 * value2;
     }
-    if (operation === "÷") {
-        result = parsed1 / parsed2;
+    if (operation === '÷') {
+        result = value1 / value2;
     }
 
-    result.toFixed(2);
-};
+    resultStr = result.toString();
 
-// function getOperation(item) {
-//     const operation = item.textContent;
-//     console.log(operation);
-//     return operation;
-// };
+    if (resultStr.includes('.')) {
+        result = result.toFixed(2);
+    }
+}
 
-
-// function normalizer(toNormalize) {
-
-//     if (typeof toNormalize === "object") {
-//         const normalized = toNormalize.filter(item => {
-//             return !isNaN(item);
-//         });
-
-//         return normalized;
-//     } else if (typeof toNormalize === "string") {
-//         const normalized = toNormalize.join("");
-//         return normalized;
-//     }
-
-//     console.log(normalized);
-//     return normalized;
-// };
+/* ---------------------------------------------- DECLARE FUNCTIONS END --------------------------------------- */
